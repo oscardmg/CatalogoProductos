8cddd60ef88058545bddb0fc9a6289ac91f663c4
Oscar Montoya <oscardmg@gmail.com>
2017-11-11 10:45:46 -0500

    adicionar metodo a servicio firebase obtener acerca de nosotros

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,17 +7,33 @@ export class InformacionService {
 
   info: any = {};
   cargada: boolean = false;
+  cargada_sobre_nosostros: boolean = false;
+
+  equipo:any = [];
 
 
   constructor(
     public http: Http
   ) {
-    // de donde obtengo la info
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.http.get("assets/data/info.pagina.json")
       .subscribe( data => {
         // console.log(data.json());
         this.info = data.json();
         this.cargada = true;
+      });
+  }
+
+  public carga_sobre_nosotros() {
+    this.http.get("https://catalogoproductos-33228.firebaseio.com/equipo.json")
+      .subscribe( data => {
+        console.log(data.json());
+        this.cargada_sobre_nosostros = true;
+        this.equipo = data.json();
       });
   }
 
